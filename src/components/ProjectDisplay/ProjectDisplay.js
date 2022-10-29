92782fb217cd3720abc978de02a71155186c49e7
GRANT NATIONS <u1247784@umail.utah.edu>
2022-10-29 11:38:49 -0600

    add rest of photos and placeholder vals for projects

--- src/components/ProjectDisplay/ProjectDisplay.js
+++ src/components/ProjectDisplay/ProjectDisplay.js
@@ -4,7 +4,7 @@ function ProjectDisplay({startDate, endDate, name, number, description, images})
     return (
         <div className="project-display">
             <div>
-                <h4 className="project-name">{number}. {name} <span className="project-dates">| {startDate} — {endDate}</span></h4>
+                <h4 className="project-name">{number}. {name} <span className="project-dates">| {startDate}{endDate ? " — " : ""}{endDate}</span></h4>
                 <p className="section-text project-description">{description}</p>
             </div>
             <div className="project-images-corral">
--- src/sections/ProjectsSection/ProjectsSection.js
+++ src/sections/ProjectsSection/ProjectsSection.js
@@ -13,6 +13,13 @@ import pump_demo from "./photos/pump-demo.jpg";
 import pump_transmission from "./photos/pump-transmission.jpg";
 import tach_mount_1 from "./photos/tach-mount-1.jpg";
 import tach_mount_2 from "./photos/tach-mount-2.JPG";
+import pupil_lect from "./photos/pupil-lectures.jpg";
+import pupil_landing from "./photos/pupil-landing.jpg";
+import react_home from "./photos/react-home.jpg";
+import react_signup from "./photos/react-signup.jpg";
+import hiragana_pdf from "./photos/hiragana-shuffle-pdf.jpg";
+import kana_shuffle from "./photos/kana-shuffle-home.jpg";
+import jetta from "./photos/jetta.jpg";
 
 function ProjectsSection({exitSection}) {
 
@@ -41,22 +48,39 @@ function ProjectsSection({exitSection}) {
         }
     ];
     const pumpPhotos = [
-        {img: pump_demo, name: "pump_demo", caption: "Performing surgery on The Crustang after its accident"},
+        {img: pump_demo, name: "pump_demo", caption: "Our pump moving water in the final competition."},
         {
             img: pump_transmission,
             name: "pump_transmission",
-            caption: "Performing surgery on The Crustang after its accident"
+            caption: "The transmission that I built for the pump."
         },
-        {img: impeller_pump, name: "impeller_pump", caption: "Performing surgery on The Crustang after its accident"}
+        {img: impeller_pump, name: "impeller_pump", caption: "The 3D printed impeller and housing that I designed."}
     ];
     const metalWorkPhotos = [
-        {img: mill, name: "mill", caption: "Performing surgery on The Crustang after its accident"},
-        {img: lathe, name: "lathe", caption: "Performing surgery on The Crustang after its accident"}
+        {img: mill, name: "mill", caption: "Cutting steel pipe to use on my 4Runner's bumper."},
+        {img: lathe, name: "lathe", caption: "Machining a door lock pin for my 1966 mustang."}
     ];
     const tachPhotos = [
-        {img: tach_mount_1, name: "tach_mount_1", caption: "Performing surgery on The Crustang after its accident"},
-        {img: tach_mount_2, name: "tach_mount_2", caption: "Performing surgery on The Crustang after its accident"}
+        {img: tach_mount_1, name: "tach_mount_1", caption: "Half of my first tachometer mount design."},
+        {img: tach_mount_2, name: "tach_mount_2", caption: "My second tachometer mount design being printed."}
     ];
+    const pupilPhotos = [
+        {img: pupil_lect, name: "pupil_lectures", caption: "A mock-up for a student's lectures view."},
+        {img: pupil_landing, name: "pupil_landing", caption: "Pupil landing page."}
+    ]
+
+    const kanaPhotos = [
+    {img: kana_shuffle, name: "kana_shuffle", caption: "Kana Shuffle web application."},
+    {img: hiragana_pdf, name: "pupil_landing", caption: "A shuffled hiragana practice sheet."}
+    ]
+
+    const reactPhotos = [
+        {img: react_home, name: "react_home", caption: "The home page for the fake brand Crown Clothing."},
+        {img: react_signup, name: "react_signup", caption: "Crown Clothing signup page."}
+    ]
+    const jettaPhotos = [
+        {img: jetta, name: "jetta", caption: "Engine swap in progress."}
+    ]
 
     return (
         <div className="section projects-section">
@@ -67,14 +91,33 @@ function ProjectsSection({exitSection}) {
             <p className="section-text">Mechanical to online and everything in between. Sorted from coolest to
                 least cool. </p>
             <div>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="Space Domain Awareness Scheduler"
+                                description={mustangDescription}
+                                number={"1"} images={[]}/>
                 <ProjectDisplay startDate="2015" endDate="2019" name="The Crustang and Dani California"
-                                description={mustangDescription} number="1" images={mustangPhotos}/>
+                                description={mustangDescription} number="2" images={mustangPhotos}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="Pupil" description={mustangDescription}
+                                number={"TODO"} images={pupilPhotos}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="Platform" description={mustangDescription}
+                                number={"TODO"} images={[]}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="Kana Shuffle" description={mustangDescription}
+                                number={"TODO"} images={kanaPhotos}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="This Portfolio Website"
+                                description={mustangDescription}
+                                number={"TODO"} images={[]}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="React E-Commerce Application"
+                                description={mustangDescription}
+                                number={"TODO"} images={reactPhotos}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="Jetta Engine Swap"
+                                description={mustangDescription}
+                                number={"TODO"} images={jettaPhotos}/>
                 <ProjectDisplay startDate="TODO" endDate="TODO" name="Impeller Pump" description={mustangDescription}
                                 number={"TODO"} images={pumpPhotos}/>
-                <ProjectDisplay startDate="TODO" endDate="TODO" name="Machining Projects" description={mustangDescription}
-                                number={"TODO"} images={metalWorkPhotos}/>
                 <ProjectDisplay startDate="TODO" endDate="TODO" name="Tachometer Mount" description={mustangDescription}
                                 number={"TODO"} images={tachPhotos}/>
+                <ProjectDisplay startDate="TODO" endDate="TODO" name="Machining Projects"
+                                description={mustangDescription}
+                                number={"TODO"} images={metalWorkPhotos}/>
             </div>
         </div>
     )
