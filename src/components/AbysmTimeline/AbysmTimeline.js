f7d5a733db6a05badd3e83b31b913f401ca0506f
GRANT NATIONS <u1247784@umail.utah.edu>
2023-10-15 12:23:57 -0600

    fix abysm

--- src/components/AbysmTimeline/AbysmTimeline.js
+++ src/components/AbysmTimeline/AbysmTimeline.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 import "./AbysmTimeline.css";
 
 const AbysmTimeline = () => {
     return (
         <div className='abysm-timeline'>
-            <TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="abysm_bot"
+            <TwitterTweetEmbed
+                // sourceType="profile"
+                // screenName="abysm_bot"
+                tweetId={'1708680194977104214'}
                 options={{
-                    tweetLimit: "10",
-                    width: "100%",
-                    height: "276px",
+                    // tweetLimit: "10",
+                    // width: "100%",
+                    // height: "276px",
+                    theme: "dark"
                 }}
-                noHeader="true"
-                theme="dark"
-                noScrollbar="true"/>
+                // noHeader="true"
+                // theme="dark"
+                // noScrollbar="true"
+                />
         </div>
     );
 }
